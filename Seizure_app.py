9ed3bc1c357822b4bf15bd9ac8e675075825c77a
Christine Hsieh <52644190+ChristineHsieh1231@users.noreply.github.com>
2026-03-24 15:37:27 +0800

    Update Seizure_app.py

--- Seizure_app.py
+++ Seizure_app.py
@@ -215,7 +215,7 @@ if uploaded_file is not None:
         st.plotly_chart(fig, use_container_width=True)
 
 # --- 2. RENDER THE PLOTS AT THE BOTTOM ---
-
+    plt.rcParams["figure.figsize"] = (15, 8)
     with plot_container:
         st.write("### Clinical EEG Preview (Raw Data)")
         
